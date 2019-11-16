b403b4e7c0d68329647b445bcb1b489a267c8c97
kevin <kevin@test.com>
2019-11-16 18:17:53 +0800

    resolve package warning list

--- WebAppDemo/Startup.cs
+++ WebAppDemo/Startup.cs
@@ -55,7 +55,7 @@ namespace WebAppDemo
 //                        (_) => "该项内容不能为空！"
 //                    );
                 }
-            ).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            ).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             services.ConfigureApplicationCookie(options =>
             {
